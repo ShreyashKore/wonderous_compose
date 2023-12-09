b5223af10ce0bfda3b220a36a6e5ca5ede4524c2
Swikruti Kore <swikrutikore@gmail.com>
2023-12-10 01:40:14 +0530

    implement search in artifact list screen

--- shared/src/commonMain/kotlin/ui/screens/ArtifactListScreen.kt
+++ shared/src/commonMain/kotlin/ui/screens/ArtifactListScreen.kt
@@ -1,21 +1,29 @@
 package ui.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DockedSearchBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,36 +32,49 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import dev.icerock.moko.mvvm.compose.getViewModel
+import dev.icerock.moko.mvvm.compose.viewModelFactory
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
 import models.Wonder
+import ui.composables.BackButton
+import ui.theme.Raleway
+import ui.theme.TenorSans
+import ui.theme.accent1
+import ui.theme.black
+import ui.theme.white
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ArtifactListScreen(
-    wonder: Wonder
+    wonder: Wonder,
+    onBackClick: () -> Unit,
 ) {
+    val viewModel = getViewModel(wonder, viewModelFactory { ArtifactListViewModel(wonder) })
+
     Column(
-        Modifier.background(color = Color.DarkGray),
+        Modifier.background(color = black),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-            IconButton(
-                onClick = {},
-                Modifier.align(alignment = Alignment.CenterStart)
-            ) {
-                Icon(Icons.Rounded.ArrowBack, contentDescription = "back")
-            }
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text("BROWSE ARTIFACTS", fontSize = 12.sp)
-                Text(wonder.title, color = Color.Yellow, fontSize = 14.sp)
-            }
+        CenterAlignedTopAppBar(
+            title = {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text("BROWSE ARTIFACTS", fontSize = 12.sp, color = white, fontFamily = Raleway)
+                    Spacer(modifier = Modifier.height(6.dp))
+                    Text(wonder.title, color = accent1, fontSize = 18.sp, fontFamily = TenorSans)
+                }
+            },
+            navigationIcon = {
+                BackButton(onClick = onBackClick)
+            },
+            colors = TopAppBarDefaults.topAppBarColors(Color.Transparent)
+        )
 
-        }
-        TextField(
+        DockedSearchBar(
             modifier = Modifier.fillMaxWidth().padding(8.dp),
-            value = "",
-            onValueChange = {
-
+            query = viewModel.searchText,
+            onQueryChange = { query ->
+                viewModel.onChangeQuery(query)
             },
             leadingIcon = {
                 Icon(Icons.Rounded.Search, contentDescription = "search")
@@ -61,21 +82,51 @@ fun ArtifactListScreen(
             placeholder = {
                 Text("Search (ex. type or material)")
             },
-            shape = RoundedCornerShape(6.dp)
+            shape = RoundedCornerShape(8.dp),
+            active = viewModel.searchActive,
+            onActiveChange = viewModel::onChangeActive,
+            onSearch = { query ->
+                viewModel.onTapSuggestion(suggestion = query)
+            },
+            content = {
+                Text(
+                    "SUGGESTIONS",
+                    modifier = Modifier.padding(horizontal = 12.dp, vertical = 4.dp)
+                )
+                Divider()
+                Column(
+                    modifier = Modifier.heightIn(max = 250.dp).verticalScroll(rememberScrollState())
+                ) {
+                    for (suggestion in viewModel.suggestions) {
+                        Text(
+                            suggestion,
+                            modifier = Modifier.fillMaxWidth()
+                                .clickable { viewModel.onTapSuggestion(suggestion) }
+                                .padding(horizontal = 12.dp, vertical = 2.dp)
+                        )
+                    }
+                }
+            },
         )
-        Text("336 artifacts found, 72 in timo")
+        Text(
+            "${wonder.searchData.count()} artifacts found",
+            color = accent1,
+            modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+        )
+
+        // Artifacts list
         LazyVerticalStaggeredGrid(
             modifier = Modifier.weight(1f),
             columns = StaggeredGridCells.Fixed(2)
         ) {
-            items(wonder.searchData.size) { i ->
-                val artifact = wonder.searchData[i]
+            items(viewModel.filteredArtifacts) { artifact ->
+
                 KamelImage(
                     asyncPainterResource(artifact.imageUrl),
                     contentDescription = artifact.title,
                     modifier = Modifier.padding(8.dp)
                         .aspectRatio(
-                            artifact.aspectRatio.toFloat()
+                            maxOf(0.5f, artifact.aspectRatio.toFloat())
                         )
                         .clip(RoundedCornerShape(6.dp)),
                     contentScale = ContentScale.FillWidth
---
+++ shared/src/commonMain/kotlin/ui/screens/ArtifactListViewModel.kt
@@ -0,0 +1,45 @@
+package ui.screens
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import dev.icerock.moko.mvvm.viewmodel.ViewModel
+import models.Wonder
+
+class ArtifactListViewModel(wonder: Wonder) : ViewModel() {
+
+    private val allSuggestions = wonder.searchSuggestions
+    private val allArtifacts = wonder.searchData
+
+    var suggestions by mutableStateOf(allSuggestions)
+        private set
+    var searchActive by mutableStateOf(false)
+        private set
+    var searchText by mutableStateOf("")
+        private set
+    var filteredArtifacts by mutableStateOf(allArtifacts)
+        private set
+
+    fun onChangeActive(isActive: Boolean) {
+        searchActive = isActive
+    }
+
+    fun onChangeQuery(query: String) {
+        searchText = query
+        suggestions = if (searchText.trim() == "") {
+            allSuggestions
+        } else {
+            allSuggestions.filter { suggestion ->
+                suggestion.contains(searchText.lowercase())
+            }
+        }
+    }
+
+    fun onTapSuggestion(suggestion: String) {
+        searchActive = false
+        filteredArtifacts = allArtifacts.filter { artifact ->
+            artifact.title.lowercase().contains(suggestion) ||
+                    artifact.keywords.lowercase().contains(suggestion)
+        }
+    }
+}

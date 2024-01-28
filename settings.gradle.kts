9f9b140257beb735ddb67bb6d68bb92e5ab82265
Shreyash Kore <kore.shreyash@gmail.com>
2024-01-28 16:04:40 +0530

    change project structure to match to the one generated by KMP Wizard

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,32 +1,12 @@
-rootProject.name = "Wonderous_Compose"
-
-include(":androidApp")
-include(":shared")
-include(":desktopApp")
+rootProject.name = "WonderousCompose"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
+        gradlePluginPortal()
         mavenCentral()
-        maven("https://jogamp.org/deployment/maven")
-    }
-
-    plugins {
-        val kotlinVersion = extra["kotlin.version"] as String
-        val agpVersion = extra["agp.version"] as String
-        val composeVersion = extra["compose.version"] as String
-
-        kotlin("jvm").version(kotlinVersion)
-        kotlin("multiplatform").version(kotlinVersion)
-        kotlin("android").version(kotlinVersion)
-        kotlin("plugin.serialization").version(kotlinVersion)
-
-        id("com.android.application").version(agpVersion)
-        id("com.android.library").version(agpVersion)
-
-        id("org.jetbrains.compose").version(composeVersion)
     }
 }
 
@@ -38,3 +18,5 @@ dependencyResolutionManagement {
         maven("https://jogamp.org/deployment/maven")
     }
 }
+
+include(":composeApp")

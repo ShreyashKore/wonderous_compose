125d17df22757cba5d913d9daf5e20702ca96833
Shreyash Kore <kore.shreyash@gmail.com>
2024-02-25 23:16:52 +0530

    implement MapView for WASM target

--- composeApp/src/wasmJsMain/kotlin/ui/composables/MapView.kt
+++ composeApp/src/wasmJsMain/kotlin/ui/composables/MapView.kt
@@ -1,12 +1,11 @@
 package ui.composables
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.hamama.kwhi.HtmlView
+import leaflet.L
+import leaflet.setupMap
 import models.GpsPosition
 
 @Composable
@@ -18,12 +17,15 @@ actual fun MapView(
     zoomLevel: Float,
     mapType: MapType
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+    HtmlView(
         modifier = modifier,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text("Map Not implemented")
-        Text("Lat: ${gps.latitude}, Lng: ${gps.longitude}")
-    }
+        factory = {
+            createElement("div").apply {
+                id = "map"
+            }
+        },
+        update = {
+            setupMap(L, "map", gps.latitude, gps.longitude, 260 * zoomLevel, 13f)
+        },
+    )
 }

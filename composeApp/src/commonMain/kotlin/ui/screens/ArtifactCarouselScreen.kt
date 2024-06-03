777d998f117a33c47e4c44772702fd64ee9c106e
Shreyash Kore <kore.shreyash@gmail.com>
2024-06-04 02:31:41 +0530

    fix ArtifactCarousel issue clipping issue; add PrevNextNavigation to ArtifactCarousel

--- composeApp/src/commonMain/kotlin/ui/screens/ArtifactCarouselScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/screens/ArtifactCarouselScreen.kt
@@ -1,7 +1,7 @@
 package ui.screens
 
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
@@ -14,14 +14,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
@@ -38,7 +40,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
@@ -52,17 +53,18 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.ScaleFactor
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.lerp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import data.HighlightData
 import kotlinx.coroutines.launch
+import models.ChichenItza
 import models.Wonder
-import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.ui.tooling.preview.Preview
 import ui.composables.AppIconButton
 import ui.composables.LongButton
+import ui.composables.PreviousNextNavigation
 import ui.theme.TenorSans
 import ui.theme.greyMedium
 import ui.theme.greyStrong
@@ -72,9 +74,10 @@ import wonderouscompose.composeapp.generated.resources.Res
 import wonderouscompose.composeapp.generated.resources.icon_search
 import kotlin.math.absoluteValue
 
+
 @OptIn(
     ExperimentalFoundationApi::class,
-    ExperimentalMaterial3Api::class, ExperimentalResourceApi::class
+    ExperimentalMaterial3Api::class
 )
 @Composable
 fun ArtifactCarouselScreen(
@@ -99,6 +102,7 @@ fun ArtifactCarouselScreen(
     val currentArtifact = artifacts[pagerState.currentPage % artifacts.size]
     // background
     Box(Modifier.fillMaxSize()) {
+        // background blurred image of Artifact
         AnimatedContent(
             currentArtifact.imageUrl,
             transitionSpec = {
@@ -111,11 +115,12 @@ fun ArtifactCarouselScreen(
                 contentDescription = "background",
                 modifier = Modifier.blur(8.dp)
                     .background(greyStrong).fillMaxWidth()
-                    .fillMaxHeight(0.8f),
+                    .fillMaxHeight(),
                 contentScale = ContentScale.cropScaled(1.4f),
                 colorFilter = ColorFilter.tint(Color.Black.copy(alpha = 0.5f), BlendMode.Multiply),
             )
         }
+        /// Background bottom circle
         Box(
             modifier = Modifier.align(Alignment.BottomCenter)
                 .requiredSize(minDimension * 2)
@@ -154,73 +159,94 @@ fun ArtifactCarouselScreen(
             Modifier.weight(1f),
         ) {
             // Artifact Image
-            HorizontalPager(
+            PreviousNextNavigation(
                 modifier = Modifier
                     .align(Alignment.Center)
                     .fillMaxHeight(),
-                state = pagerState,
-                contentPadding = PaddingValues(
-                    horizontal = (maxWidth - 200.dp) / 2
-                ),
-                pageSize = PageSize.Fixed(200.dp),
-                pageSpacing = 20.dp,
-                verticalAlignment = Alignment.Top,
-            ) { pageNo ->
-                val index = pageNo % artifacts.size
-                val artifact = artifacts[index]
-                ArtifactImage(
-                    name = artifact.title,
-                    url = artifact.imageUrl,
-                    isSelected = pagerState.currentPage == pageNo,
-                    onClick = {
-                        // When clicked on current artifact; open it's details else bring the clicked artifact to center
-                        if (artifact.id == currentArtifact.id)
-                            openArtifactDetailsScreen(artifact.id)
-                        else coroutineScope.launch {
-                            pagerState.animateScrollToPage(pageNo)
-                        }
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(.7f)
-                        .graphicsLayer {
-                            val pageOffset =
-                                ((pagerState.currentPage - pageNo) + pagerState.currentPageOffsetFraction).absoluteValue
-                            translationY = lerp(0.dp, 80.dp, fraction = pageOffset).toPx()
-                        }
-                )
+                onPreviousPressed = {
+                    coroutineScope.launch {
+                        pagerState.animateScrollToPage(pagerState.currentPage - 1)
+                    }
+                },
+                onNextPressed = {
+                    coroutineScope.launch {
+                        pagerState.animateScrollToPage(pagerState.currentPage + 1)
+                    }
+                },
+                enabled = maxWidth > 600.dp,
+            ) {
+                HorizontalPager(
+                    modifier = Modifier.fillMaxHeight(),
+                    state = pagerState,
+                    contentPadding = PaddingValues(
+                        horizontal = (maxWidth - 200.dp) / 2
+                    ),
+                    pageSize = PageSize.Fixed(200.dp),
+                    pageSpacing = 20.dp,
+                    verticalAlignment = Alignment.Top,
+                ) { pageNo ->
+                    val index = pageNo % artifacts.size
+                    val artifact = artifacts[index]
+                    ArtifactImageInBox(
+                        name = artifact.title,
+                        url = artifact.imageUrl,
+                        isSelected = pagerState.currentPage == pageNo,
+                        onClick = {
+                            // When clicked on current artifact; open it's details else bring the clicked artifact to center
+                            if (artifact.id == currentArtifact.id)
+                                openArtifactDetailsScreen(artifact.id)
+                            else coroutineScope.launch {
+                                pagerState.animateScrollToPage(pageNo)
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .heightIn(max = 300.dp)
+                            .fillMaxHeight(.6f)
+                            .graphicsLayer {
+                                val pageOffset =
+                                    ((pagerState.currentPage - pageNo) + pagerState.currentPageOffsetFraction).absoluteValue
+                                translationY = lerp(0.dp, 60.dp, fraction = pageOffset).toPx()
+                            }
+                    )
+                }
             }
 
-            // Artifact title
-            AnimatedContent(
-                modifier = Modifier.align(BiasAlignment(horizontalBias = 0f, verticalBias = .8f)),
-                targetState = currentArtifact.title,
-                transitionSpec = { fadeIn() togetherWith fadeOut() },
-                contentAlignment = Alignment.Center,
-            ) { title ->
-                Text(
-                    title,
-                    fontFamily = TenorSans,
-                    fontSize = 26.sp,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.padding(horizontal = 24.dp)
+            Column(
+                Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 80.dp, start = 20.dp, end = 20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // Artifact title
+                AnimatedContent(
+                    modifier = Modifier.widthIn(max = 600.dp),
+                    targetState = currentArtifact.title,
+                    transitionSpec = { fadeIn() togetherWith fadeOut() },
+                    contentAlignment = Alignment.Center,
+                ) { title ->
+                    Text(
+                        title,
+                        fontFamily = TenorSans,
+                        fontSize = 26.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+
+                Spacer(Modifier.height(24.dp))
+
+                LongButton(
+                    label = "BROWSE ALL ARTIFACTS",
+                    onClick = openAllArtifactsScreen,
+                    modifier = Modifier.widthIn(max = 400.dp)
                 )
             }
         }
-
-        LongButton(
-            label = "BROWSE ALL ARTIFACTS",
-            onClick = openAllArtifactsScreen,
-            modifier = Modifier
-                .padding(bottom = 100.dp, start = 20.dp, end = 20.dp)
-                .widthIn(max = 400.dp)
-        )
     }
 }
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
-private fun ArtifactImage(
+private fun ArtifactImageInBox(
     name: String,
     url: String,
     onClick: () -> Unit,
@@ -228,8 +254,8 @@ private fun ArtifactImage(
     modifier: Modifier = Modifier,
 ) {
 
-    val height by animateDpAsState(if (isSelected) 300.dp else 130.dp, animationSpec = tween(800))
-    val width by animateDpAsState(if (isSelected) 200.dp else 130.dp, animationSpec = tween(800))
+    val height by animateFloatAsState(if (isSelected) 1f else .4f, animationSpec = tween(800))
+    val width by animateFloatAsState(if (isSelected) 1f else .8f, animationSpec = tween(800))
 
     Box(
         modifier,
@@ -241,7 +267,8 @@ private fun ArtifactImage(
             modifier = Modifier
                 .border(1.dp, SolidColor(white), RoundedCornerShape(percent = 100))
                 .padding(12.dp).clip(RoundedCornerShape(percent = 100)).clickable(onClick = onClick)
-                .size(DpSize(width = width, height = height)),
+                .fillMaxHeight(height)
+                .fillMaxWidth(width),
             contentScale = ContentScale.Crop,
             onLoading = {
                 // Box(
@@ -267,4 +294,11 @@ fun ContentScale.Companion.cropScaled(scale: Float = 1f) = object : ContentScale
     override fun computeScaleFactor(srcSize: Size, dstSize: Size): ScaleFactor {
         return Crop.computeScaleFactor(srcSize, dstSize) * scale
     }
+}
+
+
+@Preview
+@Composable
+private fun Preview() {
+    ArtifactCarouselScreen(ChichenItza, {}, {})
 }

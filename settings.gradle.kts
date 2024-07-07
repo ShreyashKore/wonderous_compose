da538c76ede07c304f840bba63df8e2389cff771
Shreyash Kore <kore.shreyash@gmail.com>
2024-07-07 18:38:29 +0530

    update kotlin and compose version: fixes failing WASM build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         gradlePluginPortal()
@@ -14,6 +15,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://jogamp.org/deployment/maven")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
